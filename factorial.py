e0bc6391346059413446a78278bb53ffa05f7621
Preetpal Kaur <preetpalok123@gmail.com>
2017-09-03 22:06:52 +0530

    factorial program

---
+++ factorial.py
@@ -0,0 +1,11 @@
+def  recur_factorial(n):
+	if n == 1 :
+		return n
+	else:
+		 return n * recur_factorial(n-1) 
+
+n = input("Enter the number")
+
+#for i in range (1, n+1):
+#	factorial = n*i
+print "The factorial of " , n , "is" , recur_factorial(n)
