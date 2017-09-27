5ff568c17b73c72d4d2c9ef15ecea2500d579377
Preetpal Kaur <preetpalok123@gmail.com>
2017-09-27 10:36:49 +0530

    added swaapnumber program

---
+++ swapnumbers.py
@@ -0,0 +1,7 @@
+print "Hello , Welcome in our swap program"
+
+x = input("Enter the value of x")
+y = input("Enter the value of y")
+x,y = y,x
+print("the value of x after swapping:{}" .format(x))
+print("the value of y after swapping:{}" .format(y))
