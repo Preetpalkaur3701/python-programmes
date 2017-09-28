6c235a99a82b6061e39932b4ca7fad06bdb4b32b
Preetpal Kaur <preetpalok123@gmail.com>
2017-09-28 09:07:21 +0530

    create largenumber program

---
+++ largenumber.py
@@ -0,0 +1,14 @@
+print"Here you can find whether your number is large or not"
+
+num1 = input("Enter first number")
+num2 = input("Enter second number")
+num3 = input("Enter third number")
+
+if (num1 >= num2 )and (num1 >= num3):
+	print"num1 is large"
+if (num2 >= num1)and (num2 >= num3):
+	print"num2 is large"
+if (num3 >= num1 )and (num3>= num2):
+	print"num3 is large"
+
+
