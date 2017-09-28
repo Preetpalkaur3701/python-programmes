6ab1b1e56186fff0bbb49713a7c33563560cf4d4
Preetpal Kaur <preetpalok123@gmail.com>
2017-09-28 08:53:56 +0530

    added shapes program

---
+++ shapes.py
@@ -0,0 +1,12 @@
+import turtle
+help(turtle.color)
+
+turtle.color(225)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
