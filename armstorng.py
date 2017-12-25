dac6fde1d956b17f3dae29dd3b59539ee003a31b
Preetpal Kaur <preetpalok123@gmail.com>
2017-12-25 17:38:42 +0530

    update arstorng programme

--- armstorng.py
+++ armstorng.py
@@ -1,20 +1,23 @@
+
 print "Hi"
 print "Here you can check whether your number is armstrong or not"
 def armstrong ( x , y , z ):
-   	c = (x*x*x + y*y*y + z*z*z)
-    	return c
+   		c = x*x*x + y*y*y + z*z*z
+    		return c
 
 x = input("Enter first digit")
 y = input ("Enter seconf digit")
 z = input ("Enter third digit")
-num = armstrong ( x , y , z)
+num = armstrong( x , y , z)
 
-#print ("armstrong"+str(armstrong( x , y , z )))
+#print ("armstrong"+str(armstrong(x,y,z)))
+print type(num)
+print type(str(x)+ str(y)+str(z))
+if num > 0:
+	if num == int(str(x)+ str(y)+str(z)):
+		print " this is armstrong number"
+	else:
+		print "this is not armstrong number"
+else :
+	print "this is not armstrong number"
 
-if armstrong > 0:
-	if int(str(x)+str(y)+str(z)) == num:
-		print "  this is armstrong number "
-	else :
-		print " this is not armstrong number"
-else:
-	print "negative  number  are not armstrong"
