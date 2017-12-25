addc43aa077d4583f79160c7778a8b155ebe65f5
Preetpal Kaur <preetpalok123@gmail.com>
2017-12-25 22:25:55 +0530

    to find the sum of natural numbers

---
+++ naturalnumber.py
@@ -0,0 +1,13 @@
+print "Hi ! Here you can find the sum of natural numbers"
+print "Please enter only positive numbers , negative numbers are not included in natural numbers"
+def sum(a,b):
+	c= a+b
+	return c
+a = input("Enter first number")
+b = input("Enter second number")
+print ("sum"+str(sum(a,b)))
+
+if sum > 0 :
+	print "the sum of your is natural number"
+
+
