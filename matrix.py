4c7b2451fd0fe0b96c172b0337348dfe3f330982
Preetpal Kaur <preetpalok123@gmail.com>
2017-12-28 12:43:53 +0530

    addition of two matrices

---
+++ matrix.py
@@ -0,0 +1,32 @@
+# Here a1 is empty matrix which works as a list in below function.
+print "Enter the values of first matrix"
+a1 = []
+
+for i in range(3):
+	a1.append([])
+	for j in range(3):
+		k = input()		
+		a1[i].append(k)		
+# Above append is used to repeat function again and again untill we get the answer.
+print a1
+print "Enter the values of second matrix"
+
+a2 = []
+
+for i in range(3):
+	a2.append([])
+	for j in range(3):
+		k = input()		
+		a2[i].append(k)		
+
+print a2
+c=[]
+
+for i in range(3):
+	c.append([])
+	for j in range(3):	
+		c[i].append(a1[i][j] + a2[i][j])		
+
+print c
+
+
