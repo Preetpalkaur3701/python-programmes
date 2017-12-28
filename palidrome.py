e5002ad74c5c36860104e0aea35be954d1d4643a
Preetpal Kaur <preetpalok123@gmail.com>
2017-12-28 18:57:06 +0530

    palidrome string

---
+++ palidrome.py
@@ -0,0 +1,10 @@
+# palidrome means the word is same in forward as well as in backward direction.
+print "Hi! Here you can check your string whether your string is palidrome or not"
+print "Before writing the string start and end with colons."
+
+a = input('write any word in forward direction:')
+b = input('write word in backward direction: ')
+if a == b:
+	print "String is Palidrome"
+else :
+	print "Please Try again"
