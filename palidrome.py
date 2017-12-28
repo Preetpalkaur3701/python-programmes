2e04739298c1c9b8c0b6cb9b722d92b7d9e07945
Preetpal Kaur <preetpalok123@gmail.com>
2017-12-28 19:09:09 +0530

    palidrome string

--- palidrome.py
+++ palidrome.py
@@ -3,8 +3,8 @@ print "Hi! Here you can check your string whether your string is palidrome or no
 print "Before writing the string start and end with colons."
 
 a = input('write any word in forward direction:')
-b = input('write word in backward direction: ')
-if a == b:
+rev_str = reversed(a)
+if list(a) == list(rev_str):
 	print "String is Palidrome"
 else :
 	print "Please Try again"
